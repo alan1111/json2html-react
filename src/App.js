2c94d49e1bdf576eb0951d7dd6d45ebebb02a1ae
李旋 <lixuan@iyunbao.com>
2023-06-12 11:42:01 +0800

    feat : update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import {a} from './test.js'
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>
-          {a}Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
